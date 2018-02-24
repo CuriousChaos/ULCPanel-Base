d17398ba8f995f591c9ee2dee2490377c3811ec5
CuriousChaos <36495754+CuriousChaos@users.noreply.github.com>
2018-02-24 14:56:02 -0700

    Updated.
    nothing super special, other than changing the version to match the repositories'.

--- loaddata.sh
+++ loaddata.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # do not change the version/created-by
-echo "ULCPanel v1.3 (Created by CuriousChaos)"
+echo "ULCPanel v1.0 (Created by CuriousChaos/Some Raspbian Programmer)"
 echo Universal Linux Control Panel
 echo The simplest, easiest way to control connectable devices, networks, and more!
 echo 
 echo Current Utilizer: COMPANY_NAME_HERE
 # add extra information here
 echo 
-sleep 1s
